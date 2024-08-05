9355aecf81dfbcdac83538776712a7844b80bd92
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-05 13:50:33 -0400

    Update Home.ps1

--- Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
+++ Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
@@ -1,1 +1,28 @@
+Write-Host "Checking for OSDBuilder updates..." -ForegroundColor Cyan
+Get-OSDBuilder
+Write-Host "Updating OSDBuilder..." -ForegroundColor Cyan
+OSDBuilder -Update
+Write-Host "OSDBuilder has been updated..." -ForegroundColor Cyan
 
+
+$OSDBuilderPath = Read-Host -Prompt 'Please enter a path other than on Drive C: to use as a temporary working directory'
+OSDBuilder -SetPath $OSDBuilderPath 
+
+Write-Host "Importing Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
+Import-OSMedia -ImageName 'Windows 10 Home' -SkipGrid
+
+Write-Host "Updating Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
+Import-OSMedia -Edition Core -SkipGrid -Update -BuildNetFX
+
+Write-Host "Downloading Feature Updates and Updating Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
+OSDBuilder -Download FeatureUpdates
+Import-OSMedia -Edition Core -SkipGrid 
+
+Write-Host "Downloading OS Updates and Updating Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
+Get-OSDBuilderDownloads -Download -GridView -UpdateArch x64 -UpdateBuild 22H2 -UpdateOS "Windows 10"
+
+Write-Host "Creating OSDBuild Task for: Windows 10 Home 10.019045.4472...." -ForegroundColor Cyan
+New-OSDBuildTask -TaskName "Windows 10 Home 22H2" -CustomName "Win 10 Home 22H2" -SaveAs Task -EnableNetFX3 
+
+Write-Host "Executing NEW OSDBuild Task for: Windows 10 Home 10.019045.4472...." -ForegroundColor Cyan
+New-OSDBuild -Download -Execute -CreateISO -ByTaskName "Windows 10 Home 22H2"  
