9b677f04fc83d21e94a413821a235c27cf112f1a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-05 17:25:57 -0400

    Update Home.ps1

--- Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
+++ Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
@@ -1,13 +1,11 @@
-Write-Host "Checking for OSDBuilder updates..." -ForegroundColor Cyan
-Get-OSDBuilder
-Write-Host "Updating OSDBuilder..." -ForegroundColor Cyan
-OSDBuilder -Update
-Write-Host "OSDBuilder has been updated..." -ForegroundColor Cyan
+
 
 
 $OSDBuilderPath = Read-Host -Prompt 'Please enter a path other than on Drive C: to use as a temporary working directory'
 OSDBuilder -SetPath $OSDBuilderPath 
+pause
 OSDBuilder -CreatePaths
+pause
 
 Write-Host "Importing Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
 Import-OSMedia -ImageIndex 1 -SkipGrid
@@ -16,9 +14,16 @@ pause
 
 Write-Host "Downloading Feature Updates and Updating Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
 OSDBuilder -Download OSMediaUpdates
+pause
+Update-OSMedia -Execute
+pause
 OSDBuilder -Download FeatureUpdates
+pause
+Update-OSMedia -Execute
+pause
 OSDBuilder -Download OneDrive
-Update-OSMedia -Download -Execute
+pause
+Update-OSMedia -Execute
 
 
 pause
