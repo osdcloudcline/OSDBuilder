a74e3ca00df6dedee6646ce5406dc294da7060e0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-05 13:16:25 -0400

    Update Windows10-OSDBuild.ps1

--- Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Windows10-OSDBuild.ps1
+++ Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Windows10-OSDBuild.ps1
@@ -12,6 +12,117 @@ Write-Host '   use your Windows OS Drive (C:\)              '  -ForegroundColor 
 Write-Host '                                                '  -BackgroundColor White
 pause
 
+Function Show-MainMenu{
+   [CmdletBinding()]
+   param(
+   [string]$Title = 'OSD Builder - Windows 10 22H2 - Main Menu',
+   [string]$Question = 'What type of action do you need to do?' 
+   )
+cls
+
+Write-Host "======= $Title ======"
+Write-Host " 1. Customize Windows 10 Home"
+Write-Host " 2. Customize Windows 10 Pro"
+Write-Host " 3. Customize Windows 10 Enterprise"
+Write-Host " 4. Customize Windows 10 Enterprise VDI"
+Write-Host " 5. Customize Windows 10 Pro for Workstations"
+Write-Host " 13. Computer File Inventory"
+Write-Host " 14. Exit PowerShell"
+do 
+{
+  $selection = Read-Host 'Please choose an option'
+  switch($selection)
+  {
+  '1'{  cls
+        $Windows10Home = Invoke-WebRequest("")
+        Invoke-Expression $($Windows10Home.Content)
+      }
+  '2' { cls
+        $Domain = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/Domain%20Administration/DomainAdminMain.ps1")
+        Invoke-Expression $($Domain.Content)
+        }
+  '3' { cls
+        $Local = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/Local%20Computer/LocalPCMain.ps1")
+        Invoke-Expression $($Local.Content)
+        }
+  '4' { cls
+        $WinGetMain = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/WinGetMain.ps1")
+        Invoke-Expression $($WinGetMain.Content)
+        }
+  '5' { cls
+        $OSDCloud = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/OSD%20Cloud/OSDCloudMain.ps1")
+        Invoke-Expression $($OSDCloud.Content)
+        }
+  '6' { cls
+        $OSDBuilder = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/OSDBuilder/OSDBuilderMain.ps1")
+        Invoke-Expression $($OSDBuilder.Content)
+        }
+  '7' {cls
+      $AuditMode = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/AuditMode/MainMenu.ps1")
+      Invoke-Expression $($AuditMode.Content)
+     }
+  '8'{cls
+      $DebloatOS = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/PostInstall/OSDebloat/OSDebloatMain.ps1")
+      Invoke-Expression $($DebloatOS.Content)
+     }
+  '9'{
+        Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+        Import-Module -Name OSD -Force
+        $OSDCloudGHDownloads = "C:\OSDCloud\GitHub\downloads"
+        $UPBR = "https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe"
+        Write-Host "Downloading User Profile Backup and Restore..." -ForegroundColor Yellow
+        Write-Output "Download URL: $UPBR" -Verbose
+        Write-Output "Download Location: $OSDCloudGHDownloads" -Verbose
+        Save-WebFile -SourceUrl $UPBR -DestinationDirectory $OSDCloudGHDownloads
+        $UPBROrgName = "C:\OSDCloud\GitHub\downloads\UserProfileBackupRestore.exe"
+        $UPBRNewName = "C:\OSDCloud\GitHub\downloads\UPBR.exe"
+        Rename-Item -Path $UPBROrgName -NewName $UPBRNewName
+        Start-Process -FilePath "C:\OSDCloud\GitHub\downloads\UPBR.exe"
+        Show-MainMenu
+    }
+
+    '10'{cls
+       $AdminMain = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/Administrative/AdminMainMenu.ps1")
+       Invoke-Expression $($AdminMain.Content)
+    }
+    '11'{cls
+
+    $ScanFW = "https://pk.fail/"
+    $CheckUEFIBoot = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI PK).bytes) -match "DO NOT TRUST|DO NOT SHIP"
+    
+    Write-Verbose "Checking $Baseboard1 $Baseboard2 for PKFail security vulneralbility..." -Verbose
+    If($CheckUEFIBoot -eq $false){
+    Write-Host "Great news - $env:computername using $Baseboard1 $Baseboard2 is not affected by PKFail security vulneralbility" -ForegroundColor DarkBlue -BackgroundColor White
+    }
+    ElseIf($CheckUEFIBoot -eq $true){
+    Write-Host "Bad news - $env:computername using $Baseboard1 $Baseboard2 is affected by PKFail security vulneralbility" -ForegroundColor DarkRed -BackgroundColor White
+    Write-Verbose "Accessing Website..." -Verbose
+    Start-Process  $ScanFW
+    }
+    
+    }
+    '12'{cls
+        $OSDownloads = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/OS%20Downloads/OS.ps1")
+        Invoke-Expression $($OSDownloads.Content)
+    }
+    '13'{cls
+         Show-FileInventoryMenu
+    }
+    '14'{exit}
+    }
+    }
+    until ($selection -eq '14'){exit}
+    }
+
+
+
+
+
+
+
+
+
+
 Write-Host "Checking for OSDBuilder updates..." -ForegroundColor Cyan
 Get-OSDBuilder
 Write-Host "Updating OSDBuilder..." -ForegroundColor Cyan
@@ -21,4 +132,10 @@ Write-Host "OSDBuilder has been updated..." -ForegroundColor Cyan
 
 $OSDBuilderPath = Read-Host -Prompt 'Please enter a path other than on Drive C: to use as a temporary working directory'
 OSDBuilder -SetPath $OSDBuilderPath 
+
+
+
+
+
+
 
