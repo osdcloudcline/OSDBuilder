8c9fbd0e3dcbe668c69d735307be9b1863627cf9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-07 11:32:45 -0400

    Update Update.ps1

--- PowerShell Module/Update.ps1
+++ PowerShell Module/Update.ps1
@@ -4,3 +4,5 @@ Write-Verbose "Updating $OSDBModule..." -Verbose
 Update-Module -Name OSDBuilder -Force
 Write-Host
 Write-Verbose "Updating $OSDBModule completed..." -Verbose
+
+Import-Module -Name OSDBuilder -Force
