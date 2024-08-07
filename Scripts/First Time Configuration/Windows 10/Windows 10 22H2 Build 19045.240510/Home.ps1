752158d85d7eceee5e258b8a321ceeb4c26ada3f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-07 11:29:20 -0400

    Update Home.ps1

--- Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
+++ Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
@@ -12,29 +12,19 @@ Import-OSMedia -ImageIndex 1 -SkipGrid
 
 pause
 
-Write-Host "Downloading Feature Updates and Updating Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
+Write-Host "Downloading OS Updates, Feature Updates and Updating Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
+Get-DownOSDBuilder -Download -GridView -UpdateArch x64 -UpdateBuild 22H2 -UpdateOS "Windows 10 22H2"
+pause
+Get-DownOSDBuilder -ContentDownload 'OneDriveSetup Production'
 OSDBuilder -Download OSMediaUpdates
 pause
-Update-OSMedia -Execute
-pause
-OSDBuilder -Download FeatureUpdates
-pause
-Update-OSMedia -Execute
-pause
-OSDBuilder -Download OneDrive
-pause
-Update-OSMedia -Execute
-
-
-pause
-
-Write-Host "Downloading OS Updates and Updating Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
-Get-OSDBuilderDownloads -Download -GridView -UpdateArch x64 -UpdateBuild 22H2 -UpdateOS "Windows 10"
-
+Update-OSMedia -Download -Execute
 pause
 
 Write-Host "Creating OSDBuild Task for: Windows 10 Home 10.019045.4472...." -ForegroundColor Cyan
 New-OSDBuildTask -TaskName "Windows 10 Home 22H2" -CustomName "Win 10 Home 22H2" -SaveAs Task -EnableNetFX3 
+
+pause 
 
 Write-Host "Executing NEW OSDBuild Task for: Windows 10 Home 10.019045.4472...." -ForegroundColor Cyan
 New-OSDBuild -Download -Execute -CreateISO -ByTaskName "Windows 10 Home 22H2"  
