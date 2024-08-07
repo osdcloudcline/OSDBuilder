c64bc31744c2be72c1a5f05e8059bbd846d476b3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-07 11:06:51 -0400

    Update Update.ps1

--- PowerShell Module/Update.ps1
+++ PowerShell Module/Update.ps1
@@ -1,15 +1,5 @@
 $OSDBModule = "OSDBuilder"
 
-$OSDBuilderCheck = Get-Module -ListAvailable | Where-Object -Property Name -eq "OSDBuilder"
-Write-Verbose "Checking if $OSDBModule exists..." -Verbose
-If($OSDBuilderCheck -eq $true){
 Write-Verbose "Updating $OSDBModule..." -Verbose
 Update-Module -Name OSDBuilder -Force
-}
-ElseIf($OSDBuilderCheck -eq $false){
-Write-Verbose "Installing $OSDBModule..." -Verbose
-Install-Module -Name OSDBuilder -Force -AllowClobber -SkipPublisherCheck
-Write-Host
-Write-Verbose "Updating $OSDBModule..." -Verbose
-Update-Module -Name OSDBuilder -Force
-}
+
