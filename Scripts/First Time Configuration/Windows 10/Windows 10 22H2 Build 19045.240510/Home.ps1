d339569f59455f6a081ebc5ef57698e773b2c672
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-08 00:13:06 -0400

    Update Home.ps1

--- Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
+++ Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
@@ -13,25 +13,19 @@ Import-OSMedia -ImageIndex 1 -SkipGrid
 pause
 
 Write-Host "Downloading OS Updates, Feature Updates for Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
-Get-DownOSDBuilder -Download -GridView -UpdateArch x64 -UpdateBuild 22H2 
+Get-DownOSDBuilder -Download -FeatureUpdates -GridView -UpdateArch x64 -UpdateBuild 22H2 
 pause
 Get-DownOSDBuilder -ContentDownload 'OneDriveSetup Production'
 
 Write-Host "Updating Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
 pause
-Update-OSMedia -Download -Execute
+Update-OSMedia -Download -Execute 
 pause
 
 Write-Host "Creating OSDBuild Task for: Windows 10 Home 10.019045.4472...." -ForegroundColor Cyan
 New-OSBuildTask -TaskName "Windows 10 Home 22H2" -CustomName "Win 10 Home 22H2" -EnableNetFX3  -RemoveAppx -SaveAs Task 
 
 pause 
-
-Write-Host "Executing NEW OSBuild Task for: Windows 10 Home 10.019045.4472...." -ForegroundColor Cyan
-New-OSBuild -Download -Execute -EnableNetFX -ByTaskName "Windows 10 Home 22H2"
-
-
-pause
 
 Write-Host "Creating OS: Windows 10 Home 22H2 updated DVD ISO...."
 New-OSDBuilderISO
