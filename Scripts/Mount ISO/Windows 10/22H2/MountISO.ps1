5e03be78a31a34412b82b0acf7415c72dfc0d741
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-24 13:41:29 -0400

    Update MountISO.ps1

--- Scripts/Mount ISO/Windows 10/22H2/MountISO.ps1
+++ Scripts/Mount ISO/Windows 10/22H2/MountISO.ps1
@@ -4,13 +4,17 @@ Start-Transcript -Path $ISOLog
 
 $W1022H2 = (Test-Path -Path "C:\ISOs\OS\Clients\Win10\22H2\19045.4472.240510-1751.22H2_RELEASE_SVC_PROD2_CLIENTMULTI_X64FRE_EN-US.iso" -IsValid)
 If($W1122H2 -eq $true){
-$ISOPath = "C:\ISOs\OS\Clients\Win10\22H2\19045.4472.240510-1751.22H2_RELEASE_SVC_PROD2_CLIENTMULTI_X64FRE_EN-US.iso"
+Get-MountOS
+}
+ElseIf($W1022H2 -eq $false){
+}
+
+Function Get-MountOS(){
+$ISOPath = "C:\ISOs\OS\Clients\Win11\21H2\22000.2960.240504-2041.CO_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_EN-US.ISO"
 $ISODrive = Get-DiskImage -ImagePath $ISOPath
 $ISOLetter = ($ISODrive | Get-Volume).DriveLetter
 
 Mount-DiskImage -ImagePath $ISOPath
 $ISOLetter
-}
-ElseIf($W1022H2 -eq $false){
 }
 Stop-Transcript
