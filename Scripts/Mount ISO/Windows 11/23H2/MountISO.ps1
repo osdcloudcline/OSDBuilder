8eef6c447746ab9abbba2f49f23884ef7a38cac1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-24 14:53:59 -0400

    Update MountISO.ps1

--- Scripts/Mount ISO/Windows 11/23H2/MountISO.ps1
+++ Scripts/Mount ISO/Windows 11/23H2/MountISO.ps1
@@ -12,7 +12,7 @@ $ISOLetter
 Start-Transcript -Path $ISOLog
 
 $W1123H2 = (Test-Path -Path "C:\ISOs\OS\Clients\Win11\23H2\22631.3668.240510-1748.23H2_NI_RELEASE_SVC_PROD2_CLIENTMULTI_X64FRE_EN-US.iso" -IsValid)
-If($W1122H2 -eq $true){
+If($W1123H2 -eq $true){
 Get-MountOS
 }
 ElseIf($W1123H2 -eq $false){
