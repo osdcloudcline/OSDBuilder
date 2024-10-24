4cadd84a58e4205ca38c665ed8ca280a5c1ed19f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-24 14:53:39 -0400

    Update MountISO.ps1

--- Scripts/Mount ISO/Windows 11/24H2/MountISO.ps1
+++ Scripts/Mount ISO/Windows 11/24H2/MountISO.ps1
@@ -1,1 +1,27 @@
+$ISOLog = "C:\Logs\OSDBuilder\MountISO.log"
 
+Function Get-MountOS(){
+$ISOPath = "C:\ISOs\OS\Clients\Win11\24H2\26100.2033.241003-1823.GE_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_EN-US.ISO"
+$ISODrive = Get-DiskImage -ImagePath $ISOPath
+$ISOLetter = ($ISODrive | Get-Volume).DriveLetter
+
+Mount-DiskImage -ImagePath $ISOPath
+$ISOLetter
+}
+
+Start-Transcript -Path $ISOLog
+
+$W1124H2 = (Test-Path -Path "C:\ISOs\OS\Clients\Win11\24H2\26100.2033.241003-1823.GE_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_EN-US.iso" -IsValid)
+If($W1124H2 -eq $true){
+Get-MountOS
+}
+ElseIf($W1124H2 -eq $false){
+}
+
+Write-Host "Installing OSDBuilder Module..." -ForegroundColor Cyan
+Install-Module -Name OSDBuilder -Force -AllowClobber -SkipPublisherCheck
+Write-Host
+Write-Host "Importinmg OSDBuilder Module..." -ForegroundColor Cyan
+Import-Module -Name OSDBuilder -Force
+
+Stop-Transcript
