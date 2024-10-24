a1d67989f0d6139d29446004a632b3dcb43544fa
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-24 00:12:34 -0400

    Update MountISO.ps1

--- Scripts/Mount ISO/MountISO.ps1
+++ Scripts/Mount ISO/MountISO.ps1
@@ -2,7 +2,9 @@ $ISOLog = "C:\Logs\OSDBuilder\MountISO.log"
 
 Start-Transcript -Path $ISOLog
 
-$ISOPath = Read-Host -Prompt 'Please provide path to DVD ISO'
+$W1121H2 = (Test-Path -Path "C:\ISOs\OS\Clients\Win11\21H2\22000.2960.240504-2041.CO_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_EN-US.ISO" -IsValid)
+If($W1121H2 -eq $true)
+$ISOPath = "C:\ISOs\OS\Clients\Win11\21H2\22000.2960.240504-2041.CO_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_EN-US.ISO"
 $ISODrive = Get-DiskImage -ImagePath $ISOPath
 $ISOLetter = ($ISODrive | Get-Volume).DriveLetter
 
@@ -10,3 +12,4 @@ Mount-DiskImage -ImagePath $ISODrive
 $ISOLetter
 
 Stop-Transcript
+}
