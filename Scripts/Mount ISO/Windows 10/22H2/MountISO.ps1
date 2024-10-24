bc5a176bf6a0e8d3da0101760cfd0ac23d2acd0f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-24 14:05:36 -0400

    Update MountISO.ps1

--- Scripts/Mount ISO/Windows 10/22H2/MountISO.ps1
+++ Scripts/Mount ISO/Windows 10/22H2/MountISO.ps1
@@ -13,7 +13,7 @@ $ISOLog = "C:\Logs\OSDBuilder\MountISO.log"
 Start-Transcript -Path $ISOLog
 
 $W1022H2 = (Test-Path -Path "C:\ISOs\OS\Clients\Win10\22H2\19045.4472.240510-1751.22H2_RELEASE_SVC_PROD2_CLIENTMULTI_X64FRE_EN-US.iso" -IsValid)
-If($W1122H2 -eq $true){
+If($W1022H2 -eq $true){
 Get-MountOS
 }
 ElseIf($W1022H2 -eq $false){
