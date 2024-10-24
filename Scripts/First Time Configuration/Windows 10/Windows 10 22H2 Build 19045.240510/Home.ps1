92767cb8dd23e0fcfea29013790e170737263692
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-24 15:08:05 -0400

    Update Home.ps1

--- Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
+++ Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
@@ -1,6 +1,9 @@
 $OSDBuilderPath = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/First%20Time%20Configuration/OSDBuilder%20Configuration/Setup.ps1")
 Invoke-Expression $($OSDBuilderPath.Content)
 
+$MountISO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/Mount%20ISO/Windows%2010/22H2/MountISO.ps1")
+Invoke-Expression $($MountISO.Content)
+
 Write-Host "Downloading OS Updates, Feature Updates for Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
 Get-DownOSDBuilder -Download  -GridView -UpdateArch x64 -UpdateBuild 22H2 
 pause
