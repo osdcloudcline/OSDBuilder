c39cc5d0e1fb674afc527432862f27adea583084
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-24 22:50:54 -0400

    Update Home.ps1

--- Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
+++ Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home.ps1
@@ -4,15 +4,16 @@ Invoke-Expression $($OSDBuilderPath.Content)
 $MountISO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/Mount%20ISO/Windows%2010/22H2/MountISO.ps1")
 Invoke-Expression $($MountISO.Content)
 
+
+Write-Host "Importing Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
+Import-OSMedia -ImageIndex 1 -SkipGrid
+
+pause
+
 Write-Host "Downloading OS Updates, Feature Updates for Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
 Get-DownOSDBuilder -Download  -GridView -UpdateArch x64 -UpdateBuild 22H2 
 pause
 Get-DownOSDBuilder -ContentDownload 'OneDriveSetup Production'
-
-pause
-
-Write-Host "Importing Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
-Import-OSMedia -ImageIndex 1 -SkipGrid
 
 pause
 
@@ -21,10 +22,25 @@ pause
 Update-OSMedia -Download -Execute 
 pause
 
+Write-Host "Saving & Adding PowerShell Modules to the OSD Build..." -ForegroundColor Cyan
+$OSPoshModsDir = "$OSDBuilderPath\ContentPacks\_Global\OSPoshMods\ProgramFiles"
+$PEPoshModsDir =  "$OSDBuilderPath\ContentPacks\_Global\PEPoshMods\ProgramFiles"
+
+Save-Module -Name OSD -Path $OSPoshModsDir -Verbose
+Save-Module -Name OSD -Path $PEPoshModsDir -Verbose
+Save-Module -Name PackageManagement -Path $OSPoshModsDir -Verbose
+Save-Module -Name PackageManagement -Path $PEPoshModsDir -Verbose
+Save-Module -Name Pester -Path $OSPoshModsDir -Verbose
+Save-Module -Name PowerShellGet -Path $OSPoshModsDir -Verbose
+Save-Module -Name PowerShellGet -Path $PEPoshModsDir -Verbose
+
 Write-Host "Creating OSDBuild Task for: Windows 10 Home 10.019045.4472...." -ForegroundColor Cyan
 New-OSBuildTask -TaskName "Windows 10 Home 22H2" -CustomName "Win 10 Home 22H2" -EnableNetFX3  -RemoveAppx -SaveAs Task 
 
 pause 
+
+Write-Host "Creating New OS Build: Windows 10 Home 22H2...." -ForegroundColor Cyan
+New-OSBuildTask -Download -Execute
 
 Write-Host "Creating OS: Windows 10 Home 22H2 updated DVD ISO...."
 New-OSDBuilderISO
