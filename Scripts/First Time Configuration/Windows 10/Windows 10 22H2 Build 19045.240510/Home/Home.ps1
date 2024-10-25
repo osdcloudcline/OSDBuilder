0cdf806f18f39c1facd8caef4a9d6c40de2bd6da
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-25 09:26:07 -0400

    Create Home.ps1

---
+++ Scripts/First Time Configuration/Windows 10/Windows 10 22H2 Build 19045.240510/Home/Home.ps1
@@ -0,0 +1,43 @@
+$OSDBuilderPath = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/First%20Time%20Configuration/OSDBuilder%20Configuration/Setup.ps1")
+Invoke-Expression $($OSDBuilderPath.Content)
+
+$MountISO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/Mount%20ISO/Windows%2010/22H2/MountISO.ps1")
+Invoke-Expression $($MountISO.Content)
+
+
+Write-Host "Importing Windows 10 Home 10.019045.4472 to $OSDBuilderPath...." -ForegroundColor Cyan
+Import-OSMedia -ImageIndex 1 -SkipGrid
+
+pause
+
+Write-Host "Downloading OS Updates, Feature Updates for Windows 10 Home 10.019045.5073.241016 to $OSDBuilderPath...." -ForegroundColor Cyan
+Get-DownOSDBuilder -Download  -GridView -UpdateArch x64 -UpdateBuild 22H2 
+Get-DownOSDBuilder -ContentDownload 'OneDriveSetup Production'
+
+pause
+
+Write-Host "Updating Windows 10 Home 10.019045.5073.241016 to $OSDBuilderPath...." -ForegroundColor Cyan
+
+Update-OSMedia -Download -Execute 
+pause
+
+
+$PSModules = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/PowerShell%20Module/AddModules.ps1")
+Invoke-Expression $($PSModules.Content)
+
+
+
+Write-Host "Creating OSDBuild Task for: Windows 10 Home 10.019045.5073.241016...." -ForegroundColor Cyan
+New-OSBuildTask -TaskName "Windows 10 Home 22H2" -CustomName "Win 10 Home 22H2" -EnableNetFX3  -RemoveAppx -SaveAs Task 
+
+pause 
+
+Write-Host "Creating New OS Build: Windows 10 Home 22H2...." -ForegroundColor Cyan
+New-OSBuildTask -Download -Execute
+
+Write-Host "Creating OS: Windows 10 Home 22H2 updated DVD ISO...."
+New-OSDBuilderISO
+
+pause
+
+Show-OSDBuilderInfo 
