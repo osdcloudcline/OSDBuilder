f192acb28167510801a1cce7cfb9e36e4b1642b9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-27 17:36:14 -0400

    Update Pro.ps1

--- Scripts/First Time Configuration/Export Images/Windows 11/24H2/Pro.ps1
+++ Scripts/First Time Configuration/Export Images/Windows 11/24H2/Pro.ps1
@@ -64,39 +64,24 @@ Use-RunAs
 
 cls
 Write-Host                                                                       
-Write-Host ' This script will allow you to choose the '
-Write-Host ' downloaded from http://www.catalog.update.microsoft.com'
+Write-Host ' This script will allow you to extract the ISO and choose the '
+Write-Host ' OS WIM file to extract from the install.wim'
 Write-Host 
-Write-Host ' Please notice that the process will take quite some time, depending'
-Write-Host ' on amount and size of updates being applied to Windows image. '
 Write-Host
-Write-Host ' If you already have a bootable Windows 10 install media on USB '
-Write-Host ' flash drive, plug it in now.'
 Write-Host 
-Write-Host ' If you want to upgrade an ISO instead, mount (double click) a Windows'
-Write-Host ' ISO image and copy its content to a folder on local PC, for instance'
-Write-Host ' "D:\ISO_Files". Make sure the folder has no other content.'
 Write-Host 
-Write-Host ' When ISO files have been copied to a hard disk folder, or USB drive'
-Write-Host ' has been plugged in, press Enter to start.'
 Write-Host 
 Write-Host '                                                                      ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host ' Notice that you cannot use this script to update an ESD based install' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host ' media like for instance ISO / USB made with Media Creation Tool.     ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host ' You must first convert "install.esd" file to "install.wim". See      ' -ForegroundColor DarkBlue -BackgroundColor White
-Write-Host ' TenForums tutorial "Convert ESD to WIM":' -ForegroundColor DarkBlue -BackgroundColor White -NoNewline
-Write-Host ' http://w10g.eu/esd2wim      ' -ForegroundColor DarkCyan -BackgroundColor White
+Write-Host ' TenForums tutorial "Convert ESD to WIM":                             ' -ForegroundColor DarkBlue -BackgroundColor White -NoNewline
+Write-Host ' http://w10g.eu/esd2wim                                               ' -ForegroundColor DarkCyan -BackgroundColor White
 Write-Host '                                                                      ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host
 Write-Host ' ' -NoNewline
 pause
  
-##########################################################
-# Delete possible old log files from previous runs
-##########################################################
-
-if (Test-Path C:\OSDCloud\Logs\OSUpdates\WUSuccess.log) {Remove-Item C:\OSDCloud\Logs\OSUpdates\WUSuccess.log}
-if (Test-Path C:\OSDCloud\Logs\OSUpdates\WUFail.log) {Remove-Item C:\OSDCloud\Logs\OSUpdates\WUFail.log}
 
 ##########################################################
 # Prompt user for path to install media (USB drive) or 
@@ -131,7 +116,9 @@ Write-Host
 
 $ISOFolder = Read-Host -Prompt ' Enter source, press Enter'
 $WimFolder = $ISOFolder
-   
+
+
+
     if (Test-Path $WimFolder\Sources\install.wim)
         {
         $WimCount = 1
