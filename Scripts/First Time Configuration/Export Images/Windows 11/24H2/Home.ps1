9bc786f94d1bcabe5bacb77fdd2a09d18151c928
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-27 01:01:28 -0400

    Update Home.ps1

--- Scripts/First Time Configuration/Export Images/Windows 11/24H2/Home.ps1
+++ Scripts/First Time Configuration/Export Images/Windows 11/24H2/Home.ps1
@@ -9,3 +9,7 @@ Mount-DiskImage -ImagePath $ISOPath
 $MountedISO = Read-Host -Prompt 'Please provide path to mounted ISO and include the sources folder'
 
 Export-WindowsImage -SourceImagePath "$MountedISO\install.wim" -SourceIndex 1 -DestinationName "Windows 11 Home 24H2" -DestinationImagePath "C:\ISOs\OS\Clients\Win11\24H2\WIM Files\Home-24H2.wim"
+
+pause
+
+Dismount-DiskImage -ImagePath $ISOLetter
