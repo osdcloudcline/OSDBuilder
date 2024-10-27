f62994c8d700453317384a58adf6dcb827c42ad9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-26 22:02:12 -0400

    Update Home.ps1

--- Scripts/First Time Configuration/Windows 11/24H2/Home/Home.ps1
+++ Scripts/First Time Configuration/Windows 11/24H2/Home/Home.ps1
@@ -3,3 +3,7 @@ Invoke-Expression $($OSDBuilderPath.Content)
 
 $MountISO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/Mount%20ISO/Windows%2011/24H2/MountISO.ps1")
 Invoke-Expression $($MountISO.Content)
+
+Write-Host "Importing Windows 11 Home 24H2 Build 26100.2033.241003-1823 to $OSDBuilderPath...." -ForegroundColor Cyan
+Import-OSMedia -ImageIndex 1 -SkipGrid
+
