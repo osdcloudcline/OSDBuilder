c8f6a4d949879350b18b922ef52fb6727681875c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-27 00:44:12 -0400

    Update Home.ps1

--- Scripts/First Time Configuration/Export Images/Windows 11/24H2/Home.ps1
+++ Scripts/First Time Configuration/Export Images/Windows 11/24H2/Home.ps1
@@ -1,5 +1,7 @@
-$ISOPath = ""
+$ISOPath = "C:\ISOs\OS\Clients\Win11\24H2\26100.2033.241003-1823.GE_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_EN-US.ISO"
 $ISODrive = Get-DiskImage -ImagePath $ISOPath
 $ISOLetter = ($ISODrive | Get-Volume).DriveLetter
+
+Mount-WindowsImage -ImagePath $ISOPath -
 
 Export-WindowsImage -SourceImagePath "$ISOLetter\sources\install.wim" -SourceIndex 1 -DestinationName "Windows 11 Home 24H2" -DestinationImagePath "C:\ISOs\OS\Clients\Win11\24H2\WIM Files\Home-24H2.wim"
