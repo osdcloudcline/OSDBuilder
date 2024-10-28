8d29748e03d3bc6ff3acfa411dbc17df2540a8be
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-28 00:30:19 -0400

    Update Pro.ps1

--- Scripts/First Time Configuration/Windows 11/24H2/Pro/Pro.ps1
+++ Scripts/First Time Configuration/Windows 11/24H2/Pro/Pro.ps1
@@ -1,1 +1,2 @@
-
+$ExportOS = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/First%20Time%20Configuration/Export%20Images/Windows%2011/24H2/Pro.ps1")
+Invoke-Expression $($ExportOS.Content)
