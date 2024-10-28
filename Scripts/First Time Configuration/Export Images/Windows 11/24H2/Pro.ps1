a9e65dea3d912a78d1a22f4a39b80bc5a4f62c2f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-27 22:56:02 -0400

    Update Pro.ps1

--- Scripts/First Time Configuration/Export Images/Windows 11/24H2/Pro.ps1
+++ Scripts/First Time Configuration/Export Images/Windows 11/24H2/Pro.ps1
@@ -1,10 +1,10 @@
  $ISOPath = "C:\ISOs\OS\Clients\Win11\24H2\26100.2033.241003-1823.GE_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_EN-US.ISO"
  $MountDrive = Mount-DiskImage -ImagePath $ISOPath
  $destination = Read-Host -Prompt 'Please enter destination path'
- $WIMDestination = "$destination\WIMFile\Windows11Pro-24H2.wim"
+ $WIMDestination = "$destination\Windows11Pro-24H2.wim"
  $Drives = Get-CimInstance Win32_LogicalDisk | Where-Object -Property VolumeName -eq "CCSA_X64FRE_EN-US_DV9" 
  $OSISO = ($Drives).DeviceID
  Copy-Item -Path "$OSISO\*" -Destination $destination -Recurse
  $sourceWIM = "C:\ISOs\OS\Clients\Win11\24H2\sources\install.wim"
  Export-WindowsImage -SourceImagePath "C:\ISOs\OS\Clients\Win11\24H2\test\sources\install.wim" -SourceIndex $Index  -DestinationName 'Windows 11 Professional 24H2' -DestinationImagePath $WIMDestination
-        
+ $DismountDrive = Dismount-DiskImage -ImagePath $OSISO      
