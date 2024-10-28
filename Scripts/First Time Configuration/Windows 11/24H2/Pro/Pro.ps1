dd00fa7899432a5b3519b919b4d7aaab9e8a1388
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-28 12:42:25 -0400

    Update Pro.ps1

--- Scripts/First Time Configuration/Windows 11/24H2/Pro/Pro.ps1
+++ Scripts/First Time Configuration/Windows 11/24H2/Pro/Pro.ps1
@@ -3,3 +3,6 @@ Invoke-Expression $($ExportOS.Content)
 
 $MountISO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/Mount%20ISO/Windows%2011/24H2/MountISO.ps1")
 Invoke-Expression $($MountISO.Content)
+
+$ConfigureOSDBuilder = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/First%20Time%20Configuration/OSDBuilder%20Configuration/Setup.ps1")
+Invoke-Expression $($ConfigureOSDBuilder.Content)
