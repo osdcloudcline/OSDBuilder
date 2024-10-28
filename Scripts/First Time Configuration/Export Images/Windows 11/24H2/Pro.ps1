ee1e46203fcd636f8bac3062db596af3e278fa1b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-28 06:28:43 -0400

    Update Pro.ps1

--- Scripts/First Time Configuration/Export Images/Windows 11/24H2/Pro.ps1
+++ Scripts/First Time Configuration/Export Images/Windows 11/24H2/Pro.ps1
@@ -4,7 +4,18 @@
  $WIMDestination = "$destination\Windows11Pro-24H2.wim"
  $Drives = Get-CimInstance Win32_LogicalDisk | Where-Object -Property VolumeName -eq "CCSA_X64FRE_EN-US_DV9" 
  $OSISO = ($Drives).DeviceID
+ $WIMDir = "C:\ISOs\OS\Clients\Win11\24H2\WIM"
+ $WIMDirTest = (Test-Path -Path "C:\ISOs\OS\Clients\Win11\24H2\WIM" -IsValid)
+ If($WIMDirTest -eq $true){
  Copy-Item -Path "$OSISO\*" -Destination $destination -Recurse
  $sourceWIM = "C:\ISOs\OS\Clients\Win11\24H2\sources\install.wim"
  Export-WindowsImage -SourceImagePath "C:\ISOs\OS\Clients\Win11\24H2\sources\install.wim" -SourceIndex 3  -DestinationName 'Windows 11 Professional 24H2' -DestinationImagePath $WIMDestination
  $DismountDrive = Dismount-DiskImage -DevicePath \\.\CDROM1
+ }
+ ElseIf($WIMDirTest -eq $false){
+ New-Item -Path "C:\ISOs\OS\Clients\Win11\24H2\WIM" -ItemType Directory
+ Copy-Item -Path "$OSISO\*" -Destination $destination -Recurse
+ $sourceWIM = "C:\ISOs\OS\Clients\Win11\24H2\sources\install.wim"
+ Export-WindowsImage -SourceImagePath "C:\ISOs\OS\Clients\Win11\24H2\sources\install.wim" -SourceIndex 3  -DestinationName 'Windows 11 Professional 24H2' -DestinationImagePath $WIMDestination
+ $DismountDrive = Dismount-DiskImage -DevicePath \\.\CDROM1
+ }
