1577d93dd72fa27de0007efbd1382417302af5d9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-28 23:17:09 -0400

    Create Enterprise.ps1

---
+++ Scripts/First Time Configuration/Export Images/Windows 11/21H2/Enterprise.ps1
@@ -0,0 +1,45 @@
+ $ISOPath = "C:\ISOs\OS\Clients\Win11\21H2\22000.2960.240504-2041.CO_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_EN-US.ISO"
+ $MountDrive = Mount-DiskImage -ImagePath $ISOPath
+ $destination = "C:\ISOs\OS\Clients\Win11\21H2"
+ $WIMDestination = "$destination\Windows11Enterprise-21H2.wim"
+ $Drives = Get-CimInstance Win32_LogicalDisk | Where-Object -Property VolumeName -eq "CCSA_X64FRE_EN-US_DV9" 
+ $OSISO = ($Drives).DeviceID
+ $WIMDir = "C:\ISOs\OS\Clients\Win11\21H2\WIM"
+ $WIMDirTest = Test-Path -Path "C:\ISOs\OS\Clients\Win11\21H2\WIM" -IsValid
+ If($WIMDirTest -eq $true){
+ Copy-Item -Path "$OSISO\*" -Destination $destination -Recurse
+ $sourceWIM = "C:\ISOs\OS\Clients\Win11\21H2\sources\install.wim"
+ Export-WindowsImage -SourceImagePath "C:\ISOs\OS\Clients\Win11\21H2\sources\install.wim" -SourceIndex 6  -DestinationName 'Windows 11 Enterprise 21H2' -DestinationImagePath $WIMDestination
+ }
+ ElseIf($WIMDirTest -eq $false){
+ New-Item -Path "C:\ISOs\OS\Clients\Win11\21H2\WIM" -ItemType Directory -Force
+ Copy-Item -Path "$OSISO\*" -Destination $destination -Recurse
+ $sourceWIM = "C:\ISOs\OS\Clients\Win11\21H2\sources\install.wim"
+ Export-WindowsImage -SourceImagePath "C:\ISOs\OS\Clients\Win11\21H2\sources\install.wim" -SourceIndex 6  -DestinationName 'Windows 11 Enterprise 21H2' -DestinationImagePath $WIMDestination
+ }
+
+#######    Delete Items Variables region ########################
+
+$bootfolder = "$destination\boot"
+$EFIfolder = "$destination\efi"
+$Sourcesfolder = "$destination\sources"
+$supportfolder = "$destination\support"
+
+#####################################################
+
+Write-Host "Deleting Subfolders and Files Windows 11 24H2 Source Directory..." -ForegroundColor Cyan 
+
+rd -Path "$bootfolder" -Recurse -Force
+rd -Path "$EFIfolder" -Recurse -Force
+rd -Path "$Sourcesfolder" -Recurse -Force
+rd -Path "$supportfolder" -Recurse -Force
+
+Remove-Item -Path "$destination\*.inf" -Recurse -Force
+Remove-Item -Path "$destination\*.efi" -Recurse -Force
+Remove-Item -Path "$destination\*.exe" -Recurse -Force
+Remove-Item -Path "$destination\bootmgr" -Recurse -Force
+
+
+Write-Host "Excess Files and folders have been successfully deleted..." -ForegroundColor Green 
+
+Dismount-DiskImage -DevicePath \\.\CDROM1
