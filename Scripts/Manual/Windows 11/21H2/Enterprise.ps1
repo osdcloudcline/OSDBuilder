206ac24e1b38e0699912df718684b27244eb7389
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-28 23:23:02 -0400

    Update Enterprise.ps1

--- Scripts/Manual/Windows 11/21H2/Enterprise.ps1
+++ Scripts/Manual/Windows 11/21H2/Enterprise.ps1
@@ -1,5 +1,5 @@
 $MountISO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/Mount%20ISO/Windows%2011/21H2/MountISO.ps1")
 Invoke-Expression $($MountISO.Content)
 
-$ExportOS = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/First%20Time%20Configuration/Export%20Images/Windows%2011/21H2/Enterprise.ps1")
+$ExportOS = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/First%20Time%20Configuration/Export%20Images/Windows%2011/21H2/Enterprise/ExtractWIM.ps1")
 Invoke-Expression $($ExportOS.Content)
