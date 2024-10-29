691eb415edec126e0dd47f676d7c39cc8b65c932
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-29 00:01:55 -0400

    Update GetUpdates.ps1

--- Scripts/First Time Configuration/Export Images/Windows 11/22H2/GetUpdates.ps1
+++ Scripts/First Time Configuration/Export Images/Windows 11/22H2/GetUpdates.ps1
@@ -5,4 +5,5 @@ Update-Module -Name MSCatalog -FOrce
 $Updates = Get-MSCatalogUpdate -Search "Windows 11 Version 22H2 for x64"
 
 $Destination = Read-Host -Prompt 'Please enter destination directory to save the updates'
+New-Tem -Path $Destination -ItemType Directory
 Save-MSCatalogUpdate -Update $Update -Destination $Destination
