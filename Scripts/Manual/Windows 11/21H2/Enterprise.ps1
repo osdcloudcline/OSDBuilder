c0605315fb726d0a558b773d935b7bb6f94aed96
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-28 23:37:03 -0400

    Update Enterprise.ps1

--- Scripts/Manual/Windows 11/21H2/Enterprise.ps1
+++ Scripts/Manual/Windows 11/21H2/Enterprise.ps1
@@ -1,3 +1,9 @@
+$DISMLog = "C:\Logs\OSDBuilder\Manual\Windows11\21H2\Enterprise.log"
+
+Start-Transcript $DISMLog
+
+Write-Host "Updating Windows 11 Enterprise 21H2 Build 22000.2960.240504-2041" -ForegroundColor Cyan 
+
 $MountISO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/Mount%20ISO/Windows%2011/21H2/MountISO.ps1")
 Invoke-Expression $($MountISO.Content)
 
