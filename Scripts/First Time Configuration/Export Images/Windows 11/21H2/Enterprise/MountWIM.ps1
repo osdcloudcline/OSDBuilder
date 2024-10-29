c41fb048f7611ff8edb73b0cd750adec67e3fcf3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-28 23:32:25 -0400

    Update MountWIM.ps1

--- Scripts/First Time Configuration/Export Images/Windows 11/21H2/Enterprise/MountWIM.ps1
+++ Scripts/First Time Configuration/Export Images/Windows 11/21H2/Enterprise/MountWIM.ps1
@@ -1,2 +1,10 @@
 $source = "C:\ISOs\OS\Clients\Win11\21H2"
 $WIMSource = "$source\Windows11Enterprise-21H2.wim"
+$destination = "C:\Mount"
+
+New-Item -Path "C:\Mount" -ItemType Directory 
+
+Write-Verbose "Mounting Windows 11 Enterprise 21H2 image for editing..." -Verbose
+
+Mount-WindowsImage -ImagePath $WIMSource -Index 1 -Path $destination 
+
