ab6dd438190f7873d340f90d24e121b0699e747a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-28 23:13:55 -0400

    Update Enterprise.ps1

--- Scripts/Manual/Windows 11/21H2/Enterprise.ps1
+++ Scripts/Manual/Windows 11/21H2/Enterprise.ps1
@@ -1,1 +1,3 @@
+$MountISO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDBuilder/raw/refs/heads/main/Scripts/Mount%20ISO/Windows%2011/21H2/MountISO.ps1")
+Invoke-Expression $($MountISO.Content)
 
