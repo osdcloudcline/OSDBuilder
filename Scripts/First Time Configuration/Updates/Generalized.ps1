e7f929b50b1f96f484e1bfcd76599f27fe31170e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-31 11:25:00 -0400

    Update Generalized.ps1

--- Scripts/First Time Configuration/Updates/Generalized.ps1
+++ Scripts/First Time Configuration/Updates/Generalized.ps1
@@ -1,1 +1,79 @@
 
+
+$Destination = "E:\OSDBuilder\Updates"
+
+Write-Host
+
+# PowerShell 7.4.6 x64
+Save-MSCatalogUpdate -Guid "115a80f7-907e-4e44-8da2-438a9d365967" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+
+
+# .NET 8.0.x Updates
+Save-MSCatalogUpdate -Guid "f5601a74-ee35-4e6c-b2e2-094a00f28349" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "a80a689d-4f11-4a55-8e92-ffdc7d84f582" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "72f7e09b-b23b-49e7-9db7-c5f1c57ed664" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "75530cdd-a62c-4855-89e7-a0a2554300f5" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "87bca70f-4751-408d-bcc3-91472c7203c6" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "7c63eada-841d-4b70-8099-3c8baf329ec4" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "f5bf5124-e10f-443b-87e1-f41edb56ba1a" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "321ac36f-3e47-4b8b-a478-f3b4e2acb0e9" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+
+# .NET 7.0.x Updates
+Save-MSCatalogUpdate -Guid "bdf84773-817e-440d-bebf-fc0ab62d08ac" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "685d80b8-3d5c-4d9e-ade5-97f31f0568e7" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "b1d7e3bf-25e0-4892-9a10-e3c6a571b009" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "5da9053d-52de-4f55-ab26-93e1776035e6" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "60459cf9-bcd3-4ec6-b74e-9f000c0873a3" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "4f9d97c5-f042-4eb7-81ee-9d33d5710fd7" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "1f03a4ad-0500-4018-9fbc-5507911f316f" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "7fa675fa-d6d1-4634-9140-b1c32975ec17" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "f778af4d-1ba1-4d47-a100-f35b05aa02b0" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "724d0dae-699b-47ce-9a20-7b8ee12e3987" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "1055bec6-830f-40f4-bf76-83a16d54772d" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "908da262-f687-48e5-a7ee-1156016369d2" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "1fd124c8-8cfa-4291-b2aa-c3633260807d" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "8db982d4-b1d8-48dd-ad56-d505faf79696" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "9c70e0b1-d8b9-4e1e-ba74-0904718efcd5" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "7011fcb8-73c1-47da-b2cc-785af1feb91a" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "9ebbeb2e-5fa7-4f4b-a69d-d6742f7b3fdb" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+
+# .NET 6.0.x Updates - 2024
+Save-MSCatalogUpdate -Guid "42f40a9f-7764-4394-a37e-a01a79f91977" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "b71c2caa-70f4-4efa-9652-5634788b9713" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "d35a4a2f-af58-4450-8ffa-d39d8f90a109" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "aed741b6-7c40-46b3-8960-e2491c116c5e" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "fa355a42-1250-470a-9067-d939753738cd" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "ce0f44c6-ceac-4068-b754-e2feabdbb5c1" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "e9248bd7-e53c-4beb-ac80-1b849e415dff" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "16fe8db4-a1dd-4e7a-9c12-d0a938afbcd6" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+
+# .NET 6.0.x Updates - 2023
+Save-MSCatalogUpdate -Guid "f778af4d-1ba1-4d47-a100-f35b05aa02b0" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "724d0dae-699b-47ce-9a20-7b8ee12e3987" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "1055bec6-830f-40f4-bf76-83a16d54772d" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "908da262-f687-48e5-a7ee-1156016369d2" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "1fd124c8-8cfa-4291-b2aa-c3633260807d" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "8db982d4-b1d8-48dd-ad56-d505faf79696" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "9c70e0b1-d8b9-4e1e-ba74-0904718efcd5" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "7011fcb8-73c1-47da-b2cc-785af1feb91a" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "9ebbeb2e-5fa7-4f4b-a69d-d6742f7b3fdb" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "5da9053d-52de-4f55-ab26-93e1776035e6" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "60459cf9-bcd3-4ec6-b74e-9f000c0873a3" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+
+# .NET 6.0.x Updates - 2022
+Save-MSCatalogUpdate -Guid "4f9d97c5-f042-4eb7-81ee-9d33d5710fd7" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "1f03a4ad-0500-4018-9fbc-5507911f316f" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "7fa675fa-d6d1-4634-9140-b1c32975ec17" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "f778af4d-1ba1-4d47-a100-f35b05aa02b0" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "724d0dae-699b-47ce-9a20-7b8ee12e3987" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "1055bec6-830f-40f4-bf76-83a16d54772d" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "908da262-f687-48e5-a7ee-1156016369d2" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "1fd124c8-8cfa-4291-b2aa-c3633260807d" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "8db982d4-b1d8-48dd-ad56-d505faf79696" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "9c70e0b1-d8b9-4e1e-ba74-0904718efcd5" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "7011fcb8-73c1-47da-b2cc-785af1feb91a" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+Save-MSCatalogUpdate -Guid "9ebbeb2e-5fa7-4f4b-a69d-d6742f7b3fdb" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+
+# Windows Admin Center
+Save-MSCatalogUpdate -Guid "c73883bb-7f89-40ad-ba1e-8ef1279b9543" -Destination $Destination -Language "en-us" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+
